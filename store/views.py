3d4dafe68c3011b9c53d47753b7d9dbfa232b315
Cicada1107 <arijitdubey2018@gmail.com>
2024-12-29 22:01:57 +0530

    functionality design complete.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+import datetime
 import json
 
 # Create your views here.
@@ -77,5 +78,28 @@ def updateItem(request):
 	return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
-	print('Data:', request.body)
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+				customer =  customer,
+				order = order,
+				address = data['shipping']['address'],
+				city = data['shipping']['city'],
+				state = data['shipping']['state'],
+				zipcode = data['shipping']['zipcode'],
+			)
+	else:
+		print("User is not logged in")
 	return JsonResponse('Payment Submitted..', safe=False)
